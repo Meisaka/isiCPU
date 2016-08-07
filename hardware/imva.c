b294623a30437b4f1ae38323e8a4751d6a7baa9c
Paul Fleming <paultech@gmail.com>
2016-08-07 05:26:48 +0000

    Removed unused code :

--- hardware/imva.c
+++ hardware/imva.c
@@ -34,30 +34,6 @@ void imva_Register()
 	isi_register(&imva_Con);
 }
 
-/* how we access memory */
-#define IMVA_RD(m,a)  (m[a])
-
-#if 0
-/******* client size only ********/
-static void imva_colors(struct imva_rvstate *imva)
-{
-	uint8_t r,g,b,c;
-	uint32_t fg, bg;
-	fg = imva->colors & 0xfff;
-	c = (imva->colors & 0xf000) >> 9;
-	b = ((fg & 0xf00) >> 4 | (fg & 0xf00) >> 8);
-	g = ((fg & 0x0f0) >> 4 | (fg & 0x0f0));
-	r = ((fg & 0x00f) << 4 | (fg & 0x00f));
-	bg = ((r >> 5) + c) | (((g >> 5) + c) << 8) | (((b >> 5) + c) << 16);
-	if(c > r) r = c + 15; else r -= c;
-	if(c > g) g = c + 15; else g -= c;
-	if(c > b) b = c + 15; else b -= c;
-	fg = r | (g << 8) | (b << 16);
-	imva->fgcolor = fg;
-	imva->bgcolor = bg;
-}
-#endif
-
 int imva_reset(struct isiInfo *info)
 {
 	struct imva_rvstate *imva = (struct imva_rvstate *)info->rvstate;
@@ -125,64 +101,4 @@ static int imva_Init(struct isiInfo *info)
 	info->c = &imvaCalls;
 	return 0;
 }
-
-#if 0
-/******* client size only ********/
-/* imva is the device state
- * ram is the entire 64k words
- * rgba is a 320x200 RGBA pixel array (256000 bytes minimum)
- * slack is extra pixels to move to next line (0 if exactly sized buffer)
- */
-int imva_raster(struct imva_rvstate *imva, uint16_t *ram, uint32_t *rgba, uint32_t slack)
-{
-	uint32_t bg, fg;
-	uint16_t raddr, ova, ovo, ove;
-	bg = imva->bgcolor;
-	fg = imva->fgcolor;
-	raddr = imva->base;
-	if(!raddr) return 0; /* stand-by mode */
-
-	int omode;
-	omode = (imva->ovmode >> 4) & 3;
-	uint32_t x, y, z, ovflag, cell, of, v, vv;
-	cell = 0;
-	z = 0;
-	ovflag = 0;
-	of = 0;
-	ova = imva->ovbase;
-	ovo = raddr + imva->ovoffset;
-	ove = ovo + (40*8); /* cell line words */
-	if(!ova || imva->blink) ovo = raddr - 1;
-	for(y = 200; y--; of ^= 8) {
-		z = 0;
-		for(cell = 40; cell--; ) {
-			v = (IMVA_RD(ram,raddr) >> of) & 0x00ff;
-			if(raddr == ovo || z) {
-				vv = IMVA_RD(ram,ova + z) >> of;
-				ovflag = 1;
-				z ^= 1;
-				switch(omode) {
-				case 0: v |= vv; break;
-				case 1: v ^= vv; break;
-				case 2: v &= vv; break;
-				case 3: v = vv; break;
-				}
-			}
-			for(x = 8; x--; *(rgba++) = (v & (1 << (x & 7))) ? fg : bg);
-			raddr++;
-		}
-		rgba += slack;
-		if(!of) {
-			raddr -= 40;
-		} else {
-			if(ovflag) {
-				ova += 2;
-				ovflag = 0;
-				if(ovo != ove) ovo += 40;
-			}
-		}
-	}
-	return 1;
-}
-#endif
 
